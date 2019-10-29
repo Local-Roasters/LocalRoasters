eb050e62459bdc1aaa0493e555feaec6fa6a824a
Jackie Lin <jackie@Jackies-MacBook-Pro.local>
2019-10-29 13:02:04 -0400

    updated

--- components/route.js
+++ components/route.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { Router, Scene } from 'react-native-router-flux'
 import { ScrollView } from 'react-native'
 import Landing from '../pages/landing'
+import Home from '../pages/home'
+import Profile from '../pages/profile'
+
 
 const Routes = (props) => (
     <Router cardStyle={{ backgroundColor: 'white' }}>
         <ScrollView>
-            <Scene type="reset" key="landing" component={Landing} title="Home" initial={true} />
+            <Scene key="landing" hideNavBar={1} component={Landing}  initial={true}/>
+            <Scene type="reset" key="home" component={Home} hideNavBar={1}/>
+            <Scene type="reset" key="profile" key="profile" hideNavBar={1}  component={Profile} />
         </ScrollView>
     </Router>
 )
