6d1890d7d661bd498ae6d2f322c0f9ed0eb7debb
Mark <markgoldstein12@gmail.com>
2019-12-03 10:18:08 -0500

    added form functionality for adding a roaster

--- pages/addCoffeeShop.js
+++ pages/addCoffeeShop.js
@@ -1,30 +1,101 @@
 import React from "react";
 import {Content,CardItem,Card,Body,Left,Item} from "native-base";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, TextInput,Button, Alert, Image, Picker } from "react-native";
 import { connect } from "react-redux";
+import axios from "axios"
+import Constants from 'expo-constants';
+
 import {
   getCoffeeShopThunk,
   selectCoffeeShopThunk
 } from "../store/utilities/coffeeShop";
+import useForm from "react-hook-form"
+export default () => {
+  const { register, setValue, handleSubmit } = useForm();
+  const onSubmit = data => {
+    console.log(data)
 
-class addCoffeeShop extends React.Component {
+    let roaster = {
+      name: data.name,
+      location : {
+        streetName: data.address.split("").slice(1).join(""),
+        number:data.address.split("")[0], zip:data.zipcode
+      },
+      price:data.price,
+      rating:3,
+      coffee:{roast:"med",}
+    }
+    console.log(roaster)
+    axios.post("https://localroasters-api.herokuapp.com/roasters", roaster).then(res=> console.log(res))
+  };
+  
+  React.useEffect(() => {
+    register({ name: 'name'}, { required: true });
+    register({name: 'price'}, {required:true})
+    register({name: 'rating'}, )
+    register({name: 'address'}, {required:true})
+    register({name: "zipcode"}, {required:true})
+  }, [register])
 
-  render() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Roaster Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setValue('name', text, true)}
+      />
+      <Text style={styles.label}>Address</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setValue('address', text)}
+        placeholder={"ex: 123 Main St"}
+      />
+   <Text style={styles.label}>Price Per Cup</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setValue('price', text)}
+        placeholder={"Price for sm coffee. ie 3 or 2.5"}
+      />
+      <Text style={styles.label}>Zipcode</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setValue('zipcode', text)}
+      />
+      
 
-    return (
-      <Content style={styles.content}>
-          <Text style={styles.text}>This is where you can add your coffee shop</Text>
-      </Content>
-    );
-  }
+      <View style={styles.button}>
+        <Button style={styles.buttonInner} color title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
+    </View>
+  );
 }
 
-
-export default connect()(addCoffeeShop);
-
 const styles = StyleSheet.create({
-    text:{
-        marginTop: 50
-    }
+  label: {
+    color: 'white',
+    margin: 20,
+    marginLeft: 0
+  },
+  button :{
+    marginTop: 40,
+    color: 'white',
+    height: 40,
+    backgroundColor: '#9A764E',
+    borderRadius: 4
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 1,
+    padding: 8,
+    backgroundColor: "#036635"
+  },
+  input: {
+    backgroundColor: 'white',
 
+    height: 40,
+    padding: 10,
+    borderRadius: 4,
+  }
 });
+
