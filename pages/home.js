870d6f8f156b27b69acbef4903794c2ff9b3e2de
Mark <40143016+Maker-Mark@users.noreply.github.com>
2019-12-11 14:38:28 -0500

    Revert "Jackie"

--- pages/home.js
+++ pages/home.js
@@ -1,38 +1,38 @@
 import React from "react";
-import {Container,CardItem,Thumbnail,Footer,FooterTab,Button,Card,Body,Left, Right} from "native-base";
+import { Container, CardItem, Thumbnail, Footer, FooterTab, Button, Card, Body, Left, Right, Header } from "native-base";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { StyleSheet, TouchableOpacity, SafeAreaView, Image, FlatList, Text, TouchableHighlight, View } from "react-native";
 import axios from "axios";
 import { Actions } from "react-native-router-flux";
 import { connect } from "react-redux";
-
-import {storeCoffeeShopThunk,getCoffeeShopThunk} from "../store/utilities/coffeeShop";
-import Constants from 'expo-constants';
-// import { start } from "repl";
+import { storeCoffeeShopThunk, getCoffeeShopThunk } from "../store/utilities/coffeeShop";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       coffeeShops: [],
-      userData: {}
       sustainableCoffeeShops: [],
       sustainableFilter: false,
     };
     this.goToCoffeeShop = this.goToCoffeeShop.bind(this);
   }
   async componentDidMount() {
-    let deviceId = Constants.installationId;
+
+
+   
+          
+
+
+
+
     try {
-      let user = await axios.get(`https://localroasters-api.herokuapp.com/users/?phoneID=${deviceId}`);
-      this._isMounted = true;
-      if (this._isMounted) {
-        this.setState({
-          userData: data
-        });
-      }
       await this.props.getCoffeeShop();
+      // let { data } = await axios.get(
+      //   `https://localroasters-api.herokuapp.com/roasters/?latitude=40.678833&longitude=-73.950676`
+      // );
+
       try {
         await navigator.geolocation.getCurrentPosition(
           async position => {
@@ -42,22 +42,6 @@ class Home extends React.Component {
                 `https://localroasters-api.herokuapp.com/roasters/?latitude=${location[`coords`][`latitude`]}&longitude=${location[`coords`][`longitude`]}`
               );
               await console.log(data);
-                  let startPrice=0;
-              let endPrice=0;
-              if(user.data.price<=2){
-                endPrice=2
-              }
-              else if(user.data.price<=4){
-                endPrice=4
-                startPrice=2
-              }
-              else{
-                startPrice=6
-                endPrice=100
-              }
-              data = data.filter(coffeeShop=>{
-                return coffeeShop.price <= endPrice && coffeeShop.price >= startPrice
-              })
               this._isMounted = true;
               if (this._isMounted) {
               this.setState({
@@ -70,6 +54,7 @@ class Home extends React.Component {
               }catch(err){
                 console.log(err)
               }
+      
     } catch (err) {
       console.log(err);
     }
@@ -86,6 +71,7 @@ class Home extends React.Component {
   async goToCoffeeShop(id) {
     try {
       let select = this.state.coffeeShops.filter(a => a._id == id);
+
       await this.props.storeCoffeeShop(select[0]);
       Actions.coffeeShop();
     } catch (err) {
@@ -109,50 +95,34 @@ class Home extends React.Component {
             }}
           />
         );
-        }
-        else{
-          beans.push(<Image
-            key={i}
-            source={require("./../images/coffee-grain.png")}
-            style={{
-              height: 30,
-              width: 30,
-              flexDirection: "row",
-              marginLeft: 5
-            }}
-          />)
-        }
       }
       return (
         <Card style={styles.cardItems}>
           <CardItem>
             <Left>
-              <Image source={{ uri: img }} style={styles.thumbNail}/>
-//               <Thumbnail source={{ uri: img }} />
-//               <Body>
-//                 <Text>{name}</Text>
-//                 <Text>Price: ${price}</Text>
-//               </Body>
+              <Thumbnail source={{ uri: img }} />
+              <Body>
+                <Text>{name}</Text>
+                <Text>Price: ${price}</Text>
+              </Body>
             </Left>
-            <Right>
-                <Text style={styles.name}>{name}</Text>
-                <Text style={styles.price}>${price}</Text>
-                <Right>
+          </CardItem>
+          <CardItem>
+            <Left>
               <Button transparent textStyle={{ color: "#87838B" }}>
                 {beans}
               </Button>
+            </Left>
+            <Right>
               {sustainable ? <Ionicons name="ios-leaf" style={{ fontSize: 35, color: 'green' }} />: <View></View>}
             </Right>
-
           </CardItem>
         </Card>
       );
     }
     let i = 0;
     return (
-
       <Container>
-//       <Text style={styles.title}>Roasters Near You</Text>
         <Header style={{ backgroundColor: 'white' }}>
           <Right>
             <TouchableOpacity onPress={() => this.setState({sustainableFilter: !this.state.sustainableFilter})}>
@@ -184,8 +154,8 @@ class Home extends React.Component {
             keyExtractor={item => item._id}
           ></FlatList>
         </SafeAreaView>
-        <TouchableOpacity style={styles.addButton} onPress={()=>Actions.addCoffeeShop()}>
-            <Text style={styles.plusText}>+ Roaster</Text>
+        <TouchableOpacity style={styles.addButton} onPress={() => Actions.addCoffeeShop()}>
+          <Text style={styles.plusText}>+</Text>
         </TouchableOpacity>
         <Footer>
           <FooterTab>
@@ -224,9 +194,6 @@ const mapDispatch = dispatch => {
 export default connect(mapState, mapDispatch)(Home);
 
 const styles = StyleSheet.create({
-  top:{
-    marginTop: 40
-  },
   midText: {
     marginTop: "10%",
     marginRight: "auto",
@@ -251,7 +218,7 @@ const styles = StyleSheet.create({
     color: "white"
   },
   addButton: {
-    width: 120,
+    width: 50,
     height: 50,
     position: 'absolute',
     right: '5%',
@@ -261,32 +228,12 @@ const styles = StyleSheet.create({
   },
   plusText: {
     position: 'relative',
-    color:'white', 
-    fontSize: 20, 
-    marginRight: 'auto', 
-    marginLeft: 'auto', 
-    marginTop:"auto",
-    marginBottom:"auto",
+    color: 'white',
+    fontSize: 40,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    marginTop: "auto",
+    marginBottom: "auto",
     color: 'white'
-  },
-  price:{
-    fontSize: 16,
-    color: 'green'
-  },
-  name:{
-    fontSize: 18,
-    fontWeight: 'bold'
-  },
-  thumbNail:{
-    width: '85%',
-    height: 100,
-    borderRadius:15,
-    padding: 0
-  },
-  title:{
-    fontSize: 25,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    padding: 10
   }
 });
